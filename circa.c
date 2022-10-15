8d0a98abe523ca6bb67f6d76df9a594e8e905288
Andy Wood <andy@bitshaper.co.uk>
2022-10-15 12:09:02 +0100

    Pass configurable number of hours in which to run a specified command via the args

--- circa.c
+++ circa.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <errno.h>
+#include <float.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -7,7 +10,7 @@
 
 #define ISO8601_URL_FORMAT "%d-%02d-%02dT%02d%%3A%02d%%3A%02dZ"
 #define ISO8601_URL_SIZE   24
-#define URL_FORMAT   "https://carbon-aware-api.azurewebsites.net/emissions/forecasts/current?location=eastus&dataEndAt=%s&windowSize=%d"
+#define URL_FORMAT   "https://carbon-aware-api.azurewebsites.net/emissions/forecasts/current?location=%s&dataEndAt=%s&windowSize=%d"
 #define URL_SIZE     256
 
 struct response_t {
@@ -46,11 +49,36 @@ main(int argc, char* argv[])
   CURL *curl;
   CURLcode res;
   struct response_t response;
-  time_t now, optimal_time;
+  time_t now;
+  double wait_seconds = -DBL_MAX;
+
+  if (argc < 4) {
+    printf("Usage: %s HOURS [COMMAND [ARG]...]\n", argv[0]);
+    printf("\
+Run COMMAND at somepoint in the next few HOURS (between 1 and 24) when\n\
+local (%s) carbon intensity is at its lowest.  Assumes command will\n\
+complete within %d minute window.\n", "eastus", 30);
+    return 1;
+  }
+
+  size_t i = 1;
+  char *p;
+  int hours;
+
+  errno = 0;
+  long conv = strtol(argv[i], &p, 10);
+
+  if (errno != 0 || *p != '\0' || conv > 24 || conv < 1) {
+    fprintf(stderr, "error: hours %s must be between 1 and 24\n", argv[i]);
+    return 1;
+  } else {
+    hours = conv;
+  }
+  i++;
 
   time(&now);
   // add time this way isn't necessarily portable!
-  now = now + 2 * 60 * 60;
+  now = now + hours * 60 * 60;
   time_tm = gmtime(&now);
   snprintf(end, ISO8601_URL_SIZE, ISO8601_URL_FORMAT,
     time_tm->tm_year + 1900, // years from 1900
@@ -60,9 +88,9 @@ main(int argc, char* argv[])
     time_tm->tm_min,
     0);
 
-  printf("Requesting %d min window before %s\n", window, end);
+  printf("Requesting %d min window before %02d:%02d UTC\n", window, time_tm->tm_hour, time_tm->tm_min);
 
-  snprintf(url, URL_SIZE, URL_FORMAT, end, window);
+  snprintf(url, URL_SIZE, URL_FORMAT, "eastus", end, window);
 
   response.text = malloc(1);  /* will be grown as needed by the realloc above */
   response.size = 0;    /* no data at this point */
@@ -87,7 +115,7 @@ main(int argc, char* argv[])
       json_t *root;
       json_error_t error;
 
-      printf("%lu bytes retrieved\n", (unsigned long)response.size);
+      printf("Carbon Aware API returned %lu bytes\n", (unsigned long)response.size);
       root = json_loads(response.text, 0, &error);
 
       if(!root)
@@ -143,12 +171,12 @@ main(int argc, char* argv[])
         }
 
         timestamp_text = json_string_value(timestamp);
-        printf("optimal timestamp %s\n", timestamp_text);
+        printf("Optimal timestamp %s\n", timestamp_text);
 
         // assume fixed GMT iso8601 format
         int y,M,d,h,m,s;
         sscanf(timestamp_text, "%d-%d-%dT%d:%d:%d+00:00", &y, &M, &d, &h, &m, &s);
-        printf("optimal time %02i:%02i\n", h, m);
+        printf("Optimal time %02i:%02i UTC\n", h, m);
 
         struct tm timestamp_tm = {
           .tm_year = y - 1900, // years from 1900
@@ -161,8 +189,9 @@ main(int argc, char* argv[])
         time_t optimal_time;
         optimal_time = timegm(&timestamp_tm);    // or _mkgmtime() on windows
         time(&now);
+        wait_seconds = difftime(optimal_time, now);
         printf("Sleeping for %.2f minutes\n",
-          difftime(optimal_time, now) / 60);
+          wait_seconds / 60);
       }
     }
     curl_easy_cleanup(curl);
@@ -172,5 +201,16 @@ main(int argc, char* argv[])
 
   curl_global_cleanup();
  
-  return 0;
+  if (wait_seconds > 0) {
+    sleep(wait_seconds);
+  }
+
+  // replace this program with the command
+  execvp(argv[i], &argv[i]);
+
+  int exit_status = errno == ENOENT ? 127 : 126;  // EXIT_ENOENT : EXIT_CANNOT_INVOKE;
+  if (errno != 0) {
+    fprintf(stderr, "%s\n", strerror(errno));
+  }
+  return exit_status;
 }
